e6acf3da141f70f020bc18a62279e81db3d57aab
Alex Marey <alx.9.mar@gmail.com>
2023-12-02 21:40:19 -0500

    Day 2 part 2

--- 2023/2/main.py
+++ 2023/2/main.py
@@ -1,7 +1,7 @@
 # game: [Game #, turn data]
 def get_possible_games(game):
-    id = game[0][5:]
-    rounds = game[1].split('; ')
+    id = game[0]
+    rounds = game[1]
 
     for round in rounds:
         cubes_pulled = round.split(', ')
@@ -22,15 +22,47 @@ def get_possible_games(game):
     return int(id)
 
 
+def get_cubes_power(game):
+    rounds = game[1]
+    red = 0
+    green = 0
+    blue = 0
+    for round in rounds:
+        cubes_pulled = round.split(', ')
+        for cubes in cubes_pulled:
+            if "red" in cubes:
+                red = compare_cubes(int(cubes[:-4]), red)
+            elif "blue" in cubes:
+                blue = compare_cubes(int(cubes[:-5]), blue)
+            elif "green" in cubes:
+                green = compare_cubes(int(cubes[:-6]), green)
+    return red * green * blue
+
+
+def compare_cubes(pulled, highest):
+    if highest == 0:
+        return pulled
+    elif pulled > highest:
+        return pulled
+    return highest
+
+
 if __name__ == "__main__":
     data = list()
     with open("input.txt") as input:
         for line in input:
             line = line.strip('\n')
             game = line.split(': ')
+            game[0] = game[0][5:]
+            game[1] = game[1].split('; ')
             data.append(game)
 
     sum = 0
     for game in data:
         sum += get_possible_games(game)
-    print(sum)
+    print(f'Part 1: {sum}')
+
+    power = 0
+    for game in data:
+        power += get_cubes_power(game)
+    print(f'Part 2: {power}')
